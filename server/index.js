14fc8e5993c150365b7ef5e4fe2307eb968b6e97
lin <kdalindahung@gmail.com>
2019-02-27 13:21:37 -0800

    google auth backend (change so that the keys aren't just... there lol)

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const passport = require("passport");
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const users = require("../server/routes/api/users");
 
 const app = express();
@@ -34,13 +36,28 @@ mongoose
 const posts = require('./routes/api/posts');
 app.use('/api/posts',posts);
 
+
+passport.use(new GoogleStrategy({
+  clientID: '325287579178-odij1v2heo92ift036qn6kaacu1l9nh7.apps.googleusercontent.com',
+  clientSecret: process.env['dZSrudv7U6dHz4Zm5VwRynej'],
+  callbackURL: "http://localhost:3000/"
+},
+function(accessToken, refreshToken, profile, cb){
+  users.findOrCreate({googleId:profile.id},function(err,user){
+    return cb(err, user);
+  })
+})); 
+
 // Passport middleware
 app.use(passport.initialize());
 // Passport config
 require("../config/passport")(passport);
 // Routes
+
 app.use("/api/users", users);
 
+app.get("/login/google",
+passport.authenticate('google',{scope: 'https://www.googleapis.com/auth/plus.login'}));
 
 //Heroku, might need to change this
 const port = process.env.PORT || 5000;
