498f3b976a263a9e33a6b934ed8de6c7290f78f5
Lin Hung <kalindahung@gmail.com>
2019-04-22 11:49:16 -0700

    -

--- client/src/components/Groups/SearchGroups.js
+++ client/src/components/Groups/SearchGroups.js
@@ -1,10 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import GroupList from './GroupList';
 
 class SearchGroups extends Component {
     state={
-        query:''
+        query:'',
+        results:-1
     }
     search= e => { 
         e.preventDefault();
@@ -12,22 +14,39 @@ class SearchGroups extends Component {
         const data = {
             query: this.state.query
         }
-        axios.get('api/groups/searchgroups',  {
+        axios.get('/api/groups/searchgroups',  {
             params: data
         }).then(res=>{
-            console.log(res.data)
+            console.log(res.data);
+            this.setState({results:res.data.length});
         });
     }
     render() {
-        return (
-        <div className="search">
-        <form onSubmit={this.search}>
-            <input value={this.state.query} id="search" onChange={(e) => this.setState({ query: e.target.value })} required type="text" />
-            <button type="submit" className="btn btn-large waves-effect waves-light hoverable green accent-3">Search</button>
-       </form>
-        </div>
-        )
-        
+        const r = this.state.results;
+        if (r == -1) {
+            return (
+                <div className="search">
+                    <form onSubmit={this.search}>
+                        <input value={this.state.query} id="search" onChange={(e) => this.setState({ query: e.target.value })} required type="text" />
+                        <button type="submit" className="btn btn-large waves-effect waves-light hoverable green accent-3">Search</button>
+                    </form> 
+                </div>
+            )
+        }
+        if(r==0){
+            return(
+                <div>
+                    No results found!
+                </div>
+            )
+        }
+        else{
+            return(
+                <div>
+                    {this.state.results} results!
+                </div>
+            )
+        }
     }
 }
 
