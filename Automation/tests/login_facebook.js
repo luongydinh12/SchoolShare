768a721ea05795037d996c61c3567d5185077353
Trevor Anderson <trevoranderson1@hotmail.com>
2019-04-23 23:11:24 -0700

    Facebook login automation fix: switch to popup window to enter email/pass

--- Automation/tests/login_facebook.js
+++ Automation/tests/login_facebook.js
@@ -16,10 +16,21 @@ module.exports = {
             client.switchWindow(result.value[1])
 
           })
-        .resizeWindow(1000, 800)
-        //.assert.visible('input[type=password]')
-        //.setValue('input[type=password]', 'automator@me.com')
+        //.resizeWindow(1000, 800)
+        .assert.visible('input[type=text]')
+        .setValue('input[type=text]', 'facebook_prztpaz_test@tfbnw.net')
+        .assert.visible('input[type=password]')
+        .setValue('input[type=password]', 'cecs491a')
         .pause(1000)
+        .click('#loginbutton')
+        .windowHandles(function(result) {
+            // An array of window handles.
+            console.log(result.value);
+            client.switchWindow(result.value[0])
+          })
+        .pause(3000)
+        .click('#root > div > div:nth-child(2) > div > div > div > button:nth-child(8)')
+        .pause(2000)
         .end();
     }
   };
