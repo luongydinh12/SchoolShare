1f06859a18b7e957aa454ea50605f540b34b89f4
Trevor Anderson <trevoranderson1@hotmail.com>
2019-09-22 16:17:36 -0700

    Purge Files

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./authReducers";
-import errorReducer from "./errorReducers";
-import profileReducer from './profileReducer'
-export default combineReducers({
-  auth: authReducer,
-  errors: errorReducer,
-  profile: profileReducer,
-});
