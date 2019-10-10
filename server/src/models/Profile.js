b28ec5c4e79d7866769195f7a5c8030c8d53053c
Lin Hung <kalindahung@gmail.com>
2019-10-10 12:25:50 -0700

    getFriends populates user profiles

--- server/src/models/Profile.js
+++ server/src/models/Profile.js
@@ -47,7 +47,7 @@ ProfileSchema.statics.findByUserId=function(id,cb){
   return this.findOne({user:id}).exec(cb)
 }
 ProfileSchema.methods.getFriends = function (cb) {
-  return Friend.find({ $or: [{ profileA: this._id }, { profileB: this._id }] }).exec(cb)
+  return Friend.find({ $or: [{ profileA: this._id }, { profileB: this._id }] }).populate('profileA profileB').exec(cb)
 }
 
 var Profile = mongoose.model('profile', ProfileSchema)
