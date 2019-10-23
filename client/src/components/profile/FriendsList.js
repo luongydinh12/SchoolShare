45d310b5421be905f1f7b7a170fba7f9d92c0bcf
Lin Hung <kalindahung@gmail.com>
2019-10-23 00:32:48 -0700

    added friend search

--- client/src/components/profile/FriendsList.js
+++ client/src/components/profile/FriendsList.js
@@ -3,34 +3,62 @@ import Axios from 'axios'
 import { Link } from 'react-router-dom'
 import FriendButton from './FriendButton'
 import Spinner from "../common/Spinner"
-
 class FriendsList extends Component {
     state = {
-        friends: null
+        friends: null,
+        filteredFriends: null
     }
     componentDidMount = () => {
         this.getFriendsList()
     }
-    getFriendsList=()=>{
+
+    getFriendsList = () => {
         Axios.get('/api/friends/listFriends')
-        .then((res) => {
-            this.setState({ friends: res.data })
-        })
+            .then((res) => {
+                this.setState({ friends: res.data })
+                this.search(document.querySelector('#search').value)
+            })
     }
-    friendButtonCb=()=>{
+
+    friendButtonCb = () => {
         this.getFriendsList()
     }
-    render() {
+
+    FriendSearch = () => {
+        return (
+            <div className="search">
+                <input id="search" placeholder="Name"
+                    onChange={this.handleSearch}
+                    required type="text" />
+            </div>)
+    }
+
+    handleSearch = (e) => {
+        this.search(e.target.value)
+    }
+
+    search = (query) => {
+        if (query === null) return this.setState({ filteredFriends: null })
         const friends = this.state.friends
-        if (friends) {
-            const list = friends.map((f) => {
+        const f = friends.filter((cur) => {
+            if (cur.friend.handle.includes(query)) return cur
+        })
+        this.setState({ filteredFriends: f })
+    }
+
+    render() {
+        const { friends, filteredFriends } = this.state
+        if (friends) { //if friends list has been retrieved
+            const fr = filteredFriends ? filteredFriends : friends
+            const list = fr.map((f) => {
                 return (<ProfileListItemFragment {...f} key={f.friend._id} cb={this.friendButtonCb} />)
             })
 
             return (<div className="container" style={{ marginBottom: "20px" }}>
                 <div className="card white" style={{ padding: 5 }}>
                     <h4 className="center-text"
-                    >List of Friends</h4>
+                    >Friends List</h4>
+                    <this.FriendSearch />
                     <ul className="collection">
                         {list}
                     </ul>
@@ -39,8 +67,8 @@ class FriendsList extends Component {
         }
         return (<Spinner />)
     }
-
 }
+
 class ProfileListItemFragment extends Component {
     render() {
         const friend = this.props.friend
@@ -58,7 +86,7 @@ class ProfileListItemFragment extends Component {
                         </li>
                     </div>
                     <div className="col s2">
-                        <FriendButton profileId={friend._id} cb={this.props.cb}/>
+                        <FriendButton profileId={friend._id} cb={this.props.cb} />
                     </div>
                 </div>
             </Fragment>
