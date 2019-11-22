95e895f911be915b604293e4069c28d3028c34b0
Trevor Anderson <trevoranderson1@hotmail.com>
2019-11-21 16:42:40 -0800

    JoinGroup test

--- Automation/tests/forum.js
+++ Automation/tests/forum.js
@@ -13,7 +13,7 @@ var jobdescriptor = faker.name.jobDescriptor();
 var jobtitle = faker.name.jobTitle();
 var word = faker.random.word();
 module.exports = {
-    '@tags': ['joinGroup+createPost'],
+    '@tags': ['forum'],
     'Test School Share Login' : function (client) {
       client
       .url('https://schoolshare.me/')
@@ -60,10 +60,34 @@ module.exports = {
       .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(3) > div > div > div > div > div > div > a > img')
       .pause(1000)
       .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(3) > div > div > div > div > div > div > p:nth-child(3) > a:nth-child(2)')
-      .pause(1000)
+      .pause(5000)
       .setValue('textarea[placeholder="Type your revised message here"]', randomDescription)
       .pause(1000)
+      //revise button
       .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(3) > div > div > div > div > div > div > div:nth-child(4) > a:nth-child(2)')
+      .pause(1000)
+      //delete button
+      .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(3) > div > div > div > div > div > div > p:nth-child(3) > a:nth-child(3)')
+      .pause(1000)
+      //are you sure you want to delete
+      .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(3) > div > div > div > div > div > div > div:nth-child(4) > div > a:nth-child(1)')
+      .pause(1000)
+      //delete title
+      .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(2) > div > div > p:nth-child(3) > a:nth-child(4)')
+      //are you sure you want to delete title
+      .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(2) > div > div > div > div > a:nth-child(1)')
+      .pause(1000)
+      //click forum button
+      .click('#root > div > div > div.makeStyles-root-5 > header > div > a.MuiButtonBase-root.MuiFab-root.makeStyles-margin-1.MuiFab-extended.MuiFab-sizeSmall.MuiFab-primary > span.MuiFab-label')
+      .pause(1000)
+      //click general
+      .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(2) > div > div:nth-child(1) > a')
+      .pause(1000)
+      .click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div.row > div.col.s12 > div:nth-child(1) > a')
+      .pause(1000)
+      //click save
+      //.click('#root > div > div > div:nth-child(2) > div:nth-child(3) > div > div:nth-child(2) > p > div > h6')
+      .pause(1000)
       .pause(1000)
     }
   
